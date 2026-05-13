015394a27055769fa6b7b0fbdcf53178419a2584
Anton Ohiy <anton.ogy@gmail.com>
2026-05-13 12:25:31 +0300

    refactor(config): wrap all config env options by config methods

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { ConfigService } from '@nestjs/config';
+import { ConfigService } from '../config';
 import { PrismaClient } from '../generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor(private config: ConfigService) {
-    const adapter = new PrismaPg({
-      connectionString: `${config.get<string>('DATABASE_URL')}`,
-    });
-
+    const adapter = new PrismaPg(config.getDbOptions());
     super({ adapter });
   }
 }
